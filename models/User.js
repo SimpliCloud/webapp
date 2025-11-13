928af6e43b67c3595131a9e13547ae45481d3220
Vatsal Naik <naik.vat@northeastern.edu>
2025-11-13 14:48:26 -0500

    added api parameters

--- models/User.js
+++ models/User.js
@@ -1,158 +1,188 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
 const bcrypt = require('bcrypt');
+const { sequelize } = require('../config/database');
+const { v4: uuidv4 } = require('uuid');
 
 const User = sequelize.define('User', {
   id: {
     type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
+    defaultValue: () => uuidv4(),
     primaryKey: true,
     allowNull: false,
-    comment: 'Unique identifier for the user'
+    readOnly: true
   },
   
   email: {
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
+    unique: {
+      msg: 'Email address already exists'
+    },
     validate: {
       isEmail: {
         msg: 'Must be a valid email address'
       },
       notEmpty: {
-        msg: 'Email is required'
+        msg: 'Email cannot be empty'
       }
-    },
-    comment: 'User email address (used as username)'
+    }
   },
   
   password: {
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING,
     allowNull: false,
     validate: {
       notEmpty: {
-        msg: 'Password is required'
+        msg: 'Password cannot be empty'
       },
       len: {
-        args: [6, 255],
+        args: [6, 100],
         msg: 'Password must be at least 6 characters long'
       }
-    },
-    comment: 'Hashed password using BCrypt'
+    }
   },
   
   first_name: {
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING,
     allowNull: false,
     validate: {
       notEmpty: {
-        msg: 'First name is required'
+        msg: 'First name cannot be empty'
       }
-    },
-    comment: 'User first name'
+    }
   },
   
   last_name: {
-    type: DataTypes.STRING(255),
+    type: DataTypes.STRING,
     allowNull: false,
     validate: {
       notEmpty: {
-        msg: 'Last name is required'
+        msg: 'Last name cannot be empty'
       }
-    },
-    comment: 'User last name'
+    }
+  },
+  
+  // NEW: Email verification fields
+  email_verified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false
+  },
+  
+  verification_token: {
+    type: DataTypes.UUID,
+    allowNull: true,
+    defaultValue: null
+  },
+  
+  token_created_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: null
   },
   
   account_created: {
     type: DataTypes.DATE,
     allowNull: false,
     defaultValue: DataTypes.NOW,
-    comment: 'Timestamp when account was created'
+    readOnly: true
   },
   
   account_updated: {
     type: DataTypes.DATE,
     allowNull: false,
     defaultValue: DataTypes.NOW,
-    comment: 'Timestamp when account was last updated'
+    readOnly: true
   }
 }, {
   tableName: 'users',
-  timestamps: false, // We're managing timestamps manually
-  indexes: [
-    {
-      unique: true,
-      fields: ['email'],
-      name: 'idx_users_email'
-    }
-  ],
+  timestamps: false,
   
   hooks: {
     // Hash password before creating user
     beforeCreate: async (user) => {
       if (user.password) {
-        const salt = await bcrypt.genSalt(10);
+        const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10);
         user.password = await bcrypt.hash(user.password, salt);
       }
-      user.account_created = new Date();
+      
+      // Generate verification token on user creation
+      user.verification_token = uuidv4();
+      user.token_created_at = new Date();
+      user.email_verified = false;
+    },
+    
+    // Hash password before updating user
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
       user.account_updated = new Date();
     },
     
-    // Hash password if it's being updated
-    beforeUpdate: async (user) => {
-      if (user.changed('password')) {
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
+    // Update account_updated timestamp before save
+    beforeSave: (user) => {
+      if (!user.isNewRecord) {
+        user.account_updated = new Date();
       }
-      user.account_updated = new Date();
     }
   }
 });
 
-// Instance method to validate password
-User.prototype.validatePassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
+// Instance method to compare password
+User.prototype.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
-// Instance method to return user data without password
-User.prototype.toJSON = function() {
-  const values = Object.assign({}, this.get());
-  delete values.password;
-  return values;
-};
-
-// Static method for authentication
+// Static method to authenticate user
 User.authenticate = async function(email, password) {
-  try {
-    // Find user by email
-    const user = await this.findOne({
-      where: { email: email }
-    });
-    
-    if (!user) {
-      return null;
-    }
-    
-    // Verify password
-    const isValidPassword = await bcrypt.compare(password, user.password);
-    
-    if (!isValidPassword) {
-      return null;
-    }
-    
-    // Return user data without password
-    return {
-      id: user.id,
-      email: user.email,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      account_created: user.account_created,
-      account_updated: user.account_updated
-    };
-  } catch (error) {
-    console.error('Authentication error:', error);
+  const user = await User.findOne({ where: { email } });
+  
+  if (!user) {
     return null;
   }
+  
+  const isMatch = await user.comparePassword(password);
+  
+  if (!isMatch) {
+    return null;
+  }
+  
+  return user;
+};
+
+// Method to check if verification token is expired
+User.prototype.isTokenExpired = function() {
+  if (!this.token_created_at) {
+    return true;
+  }
+  
+  const tokenAge = Date.now() - new Date(this.token_created_at).getTime();
+  const expiryTime = parseInt(process.env.VERIFICATION_TOKEN_EXPIRY) || 60; // 60 seconds
+  
+  return tokenAge > (expiryTime * 1000);
+};
+
+// Method to generate new verification token
+User.prototype.generateVerificationToken = function() {
+  this.verification_token = uuidv4();
+  this.token_created_at = new Date();
+  return this.verification_token;
+};
+
+// Customize JSON output to exclude sensitive fields
+User.prototype.toJSON = function() {
+  const values = { ...this.get() };
+  
+  // Remove sensitive fields
+  delete values.password;
+  
+  // Don't expose internal verification fields in normal responses
+  delete values.verification_token;
+  delete values.token_created_at;
+  
+  return values;
 };
 
 module.exports = User;
